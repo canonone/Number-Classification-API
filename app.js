140baf64afd439da249f35dd033efef8a6d75a15
Christian Umoh <umohchristian001@gmail.com>
2025-02-03 11:22:38 +0100

    update

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use((req, res, next) => {
+  res.status(400).json({ number: null, error: true });
+});
 
 // API endpoint to classify a number
 app.get("/", (req, res) => {
